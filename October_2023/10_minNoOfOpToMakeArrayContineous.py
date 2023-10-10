34d3d67de74c1c0548898f36120398de66612222
gaurav-batheja <123798033+gaurav-batheja@users.noreply.github.com>
2023-10-10 17:18:44 +0530

    Create 10_minNoOfOpToMakeArrayContineous.py

---
+++ October_2023/10_minNoOfOpToMakeArrayContineous.py
@@ -0,0 +1,22 @@
+# You are given an integer array nums. In one operation, you can replace any element in nums with any integer.
+
+# nums is considered continuous if both of the following conditions are fulfilled:
+
+# All elements in nums are unique.
+# The difference between the maximum element and the minimum element in nums equals nums.length - 1.
+# For example, nums = [4, 2, 5, 3] is continuous, but nums = [1, 2, 3, 5, 6] is not continuous.
+
+# Return the minimum number of operations to make nums continuous
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        nums = sorted(set(nums))
+        ans = sys.maxsize
+
+        for i, s in enumerate(nums):
+            e = s + n - 1
+            idx = bisect_right(nums, e)
+
+            ans = min(ans, n - (idx - i))
+        return ans
